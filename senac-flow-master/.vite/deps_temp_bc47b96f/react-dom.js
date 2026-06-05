f033683e9e947db23cc27e67222551450c727479
luanhayalla2 <141869708+luanhayalla2@users.noreply.github.com>
2026-06-05 17:17:52 -0300

    chore: generate Vite dependency pre-bundling metadata and configuration files

---
+++ senac-flow-master/.vite/deps_temp_bc47b96f/react-dom.js
@@ -0,0 +1,5 @@
+import {
+  require_react_dom
+} from "./chunk-ALQL4CFF.js";
+import "./chunk-UABJVSAO.js";
+export default require_react_dom();
---
+++ senac-flow-master/.vite/deps_temp_bc47b96f/react_jsx-runtime.js
@@ -0,0 +1,5 @@
+import {
+  require_jsx_runtime
+} from "./chunk-GFAWEGDV.js";
+import "./chunk-UABJVSAO.js";
+export default require_jsx_runtime();
